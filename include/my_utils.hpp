55250e8020d5648de912e0503427cab7284513d9
arya <a.hasanli@yahoo.com>
2021-01-31 13:53:32 +0300

    Add My Utils

---
+++ include/my_utils.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+class MyUtils {
+   public:
+    MyUtils();
+
+    bool pathExists(const std::string &path);
+    static void mouseCallback(int event, int x, int y, int, void *p);
+    std::vector<cv::Point2f> pickNPoints(int n0, const cv::Mat &img, std::string msg);
+
+    ~MyUtils();
+
+   private:
+    cv::Mat bg_img;
+    std::vector<cv::Point2f> result;
+    std::string name;
+    std::atomic<bool> done;
+    int n;
+    double show_scale;
+};
