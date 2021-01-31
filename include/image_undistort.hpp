c0924af485fa6e96bd7c870a6a5c5b89aa595a7d
arya <a.hasanli@yahoo.com>
2021-01-31 13:51:04 +0300

    File reformat

--- include/image_undistort.hpp
+++ include/image_undistort.hpp
@@ -1,23 +1,21 @@
 #pragma once
 
-#include <iostream>
 #include <fstream>
-
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 class ImageUndistort {
+   public:
+    ImageUndistort();
 
-public:
-  ImageUndistort();
+    bool initialize(const cv::Size& img_size, const cv::Mat& cam_matrix, const cv::Mat& dist_coeffs);
+    bool isInitialized() const;
+    bool undistort(const cv::Mat& img_in, cv::Mat& img_out);
 
-  bool initialize(const cv::Size& img_size, const cv::Mat& cam_matrix, const cv::Mat& dist_coeffs);
-  bool isInitialized() const;
-  bool undistort(const cv::Mat& img_in, cv::Mat& img_out);
+    ~ImageUndistort();
 
-  ~ImageUndistort();
-
-private:
-  bool is_initialized_;
-  cv::Mat cam_matrix_, dist_coeffs_;
-  cv::Mat full_map1_, full_map2_;  // For faster version
+   private:
+    bool is_initialized_;
+    cv::Mat cam_matrix_, dist_coeffs_;
+    cv::Mat full_map1_, full_map2_;  // For faster version
 };
