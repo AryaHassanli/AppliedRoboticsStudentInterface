422b3698b8c59186828e02d624856e2fcc5e3c7f
arya <a.hasanli@yahoo.com>
2021-02-06 09:07:45 +0100

    Change Map Class strcuture

--- include/map.hpp
+++ include/map.hpp
@@ -6,30 +6,38 @@
 #include "my_utils.hpp"
 #include "utils.hpp"
 
+struct MaskBound {
+    cv::Scalar lower_bound;
+    cv::Scalar upper_bound;
+};
+
 class Map {
    public:
-    struct MaskBound {
-        cv::Scalar lower_bound;
-        cv::Scalar upper_bound;
-    };
-
-    bool is_initialized;
-
     Map();
 
-    void initialize();
-    void detectMasks(const cv::Mat &img_in, std::string help_text, std::vector<MaskBound> &masks);
-    static void applyMasks(const cv::Mat &img_in, std::vector<Map::MaskBound> &mask_bounds, cv::Mat &out_mask);
+    bool isInitialized() const;
+    void initialize(const cv::Mat &img_in, const std::string &config_folder);
+
+    static void createMaskBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds, std::string help_text);
+    static void applyMasks(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds, cv::Mat &out_mask);
+
+    static void getObstaclesBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds,
+                                   const std::string &config_folder);
+    static void getGateBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds,
+                              const std::string &config_folder);
+    static void getVictimsBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds,
+                                 const std::string &config_folder);
+
+    static void findObstacles(const cv::Mat &mask_in, std::vector<Polygon> &obstacles, const double scale);
+    static void findGate(const cv::Mat &mask_in, Polygon &gate, const double scale);
+    static void findVictims(const cv::Mat &mask_in, std::vector<std::pair<int, Polygon>> &victims, const double scale);
+
+    static void getRobotBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds,
+                               const std::string &config_folder);
+    static bool findRobot(const cv::Mat &mask_in, Polygon &triangle, const double scale);
 
     ~Map();
 
-    std::vector<MaskBound> obstacle_mask_bounds, victim_mask_bounds, gate_mask_bounds, robot_mask_bounds;
-    cv::Mat obstacle_mask, victim_mask, robot_mask, gate_mask;
-
    private:
-    struct Threshold {
-        uchar h = 5;
-        uchar s = 50;
-        uchar v = 50;
-    } threshold;
+    bool is_initialized;
 };
--- src/map.cpp
+++ src/map.cpp
@@ -1,5 +1,6 @@
 #include "map.hpp"
 
+#include <experimental/filesystem>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -8,13 +9,24 @@
 
 static MyUtils utils;
 
+struct Threshold {
+    uchar h = 5;
+    uchar s = 50;
+    uchar v = 50;
+} threshold;
+
 Map::Map() { is_initialized = false; }
 
-void Map::initialize() {
+bool Map::isInitialized() const { return is_initialized; }
+
+void Map::initialize(const cv::Mat &img_in, const std::string &config_folder) {
+    if (is_initialized) {
+        return;
+    }
     is_initialized = true;
 }
 
-void Map::detectMasks(const cv::Mat &img_in, std::string help_text, std::vector<MaskBound> &masks) {
+void Map::createMaskBounds(const cv::Mat &img_in, std::vector<MaskBound> &masks, std::string help_text) {
     cv::Mat hsv_img;
     cv::cvtColor(img_in, hsv_img, cv::COLOR_BGR2HSV);
 
@@ -58,7 +70,153 @@ void Map::detectMasks(const cv::Mat &img_in, std::string help_text, std::vector<
     return;
 }
 
-void Map::applyMasks(const cv::Mat &img_in, std::vector<Map::MaskBound> &mask_bounds, cv::Mat &out_mask) {
+void Map::getObstaclesBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds,
+                             const std::string &config_folder) {
+    std::string obstacle_file_path = config_folder + "/obstacle_mask_bounds.csv";
+    if (!std::experimental::filesystem::exists(obstacle_file_path)) {
+        std::ofstream output(obstacle_file_path);
+        if (!output.is_open()) {
+            throw std::runtime_error("Cannot write file: " + obstacle_file_path);
+        }
+        createMaskBounds(img_in, mask_bounds, " For Obstacles");
+
+        for (int i = 0; i < mask_bounds.size(); i++) {
+            output << mask_bounds[i].lower_bound.val[0] << " ";
+            output << mask_bounds[i].lower_bound.val[1] << " ";
+            output << mask_bounds[i].lower_bound.val[2] << " ";
+            output << mask_bounds[i].upper_bound.val[0] << " ";
+            output << mask_bounds[i].upper_bound.val[1] << " ";
+            output << mask_bounds[i].upper_bound.val[2] << std::endl;
+        }
+        output.close();
+    } else {
+        std::ifstream input(obstacle_file_path);
+        while (!input.eof()) {
+            double h1, s1, v1, h2, s2, v2;
+            if (!(input >> h1 >> s1 >> v1 >> h2 >> s2 >> v2)) {
+                if (input.eof())
+                    break;
+                else {
+                    throw std::runtime_error("Malformed file: " + obstacle_file_path);
+                }
+            }
+            MaskBound tmp_mask_bound;
+            tmp_mask_bound.lower_bound = cv::Scalar(h1, s1, v1);
+            tmp_mask_bound.upper_bound = cv::Scalar(h2, s2, v2);
+            mask_bounds.push_back(tmp_mask_bound);
+        }
+    }
+}
+void Map::getGateBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds, const std::string &config_folder) {
+    std::string gate_file_path = config_folder + "/gate_mask_bounds.csv";
+    if (!std::experimental::filesystem::exists(gate_file_path)) {
+        std::ofstream output(gate_file_path);
+        if (!output.is_open()) {
+            throw std::runtime_error("Cannot write file: " + gate_file_path);
+        }
+        Map::createMaskBounds(img_in, mask_bounds, " For gates");
+        for (int i = 0; i < mask_bounds.size(); i++) {
+            output << mask_bounds[i].lower_bound.val[0] << " ";
+            output << mask_bounds[i].lower_bound.val[1] << " ";
+            output << mask_bounds[i].lower_bound.val[2] << " ";
+            output << mask_bounds[i].upper_bound.val[0] << " ";
+            output << mask_bounds[i].upper_bound.val[1] << " ";
+            output << mask_bounds[i].upper_bound.val[2] << std::endl;
+        }
+        output.close();
+    } else {
+        std::ifstream input(gate_file_path);
+        while (!input.eof()) {
+            double h1, s1, v1, h2, s2, v2;
+            if (!(input >> h1 >> s1 >> v1 >> h2 >> s2 >> v2)) {
+                if (input.eof())
+                    break;
+                else {
+                    throw std::runtime_error("Malformed file: " + gate_file_path);
+                }
+            }
+            MaskBound tmp_mask_bound;
+            tmp_mask_bound.lower_bound = cv::Scalar(h1, s1, v1);
+            tmp_mask_bound.upper_bound = cv::Scalar(h2, s2, v2);
+            mask_bounds.push_back(tmp_mask_bound);
+        }
+    }
+}
+
+void Map::getVictimsBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds,
+                           const std::string &config_folder) {
+    std::string victim_file_path = config_folder + "/victim_mask_bounds.csv";
+    if (!std::experimental::filesystem::exists(victim_file_path)) {
+        std::ofstream output(victim_file_path);
+        if (!output.is_open()) {
+            throw std::runtime_error("Cannot write file: " + victim_file_path);
+        }
+        Map::createMaskBounds(img_in, mask_bounds, " For victims");
+        for (int i = 0; i < mask_bounds.size(); i++) {
+            output << mask_bounds[i].lower_bound.val[0] << " ";
+            output << mask_bounds[i].lower_bound.val[1] << " ";
+            output << mask_bounds[i].lower_bound.val[2] << " ";
+            output << mask_bounds[i].upper_bound.val[0] << " ";
+            output << mask_bounds[i].upper_bound.val[1] << " ";
+            output << mask_bounds[i].upper_bound.val[2] << std::endl;
+        }
+        output.close();
+    } else {
+        std::ifstream input(victim_file_path);
+        while (!input.eof()) {
+            double h1, s1, v1, h2, s2, v2;
+            if (!(input >> h1 >> s1 >> v1 >> h2 >> s2 >> v2)) {
+                if (input.eof())
+                    break;
+                else {
+                    throw std::runtime_error("Malformed file: " + victim_file_path);
+                }
+            }
+            MaskBound tmp_mask_bound;
+            tmp_mask_bound.lower_bound = cv::Scalar(h1, s1, v1);
+            tmp_mask_bound.upper_bound = cv::Scalar(h2, s2, v2);
+            mask_bounds.push_back(tmp_mask_bound);
+        }
+    }
+}
+
+void Map::getRobotBounds(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds, const std::string &config_folder) {
+    std::string robot_file_path = config_folder + "/robot_mask_bounds.csv";
+    if (!std::experimental::filesystem::exists(robot_file_path)) {
+        std::ofstream output(robot_file_path);
+        if (!output.is_open()) {
+            throw std::runtime_error("Cannot write file: " + robot_file_path);
+        }
+        Map::createMaskBounds(img_in, mask_bounds, " For robot");
+        for (int i = 0; i < mask_bounds.size(); i++) {
+            output << mask_bounds[i].lower_bound.val[0] << " ";
+            output << mask_bounds[i].lower_bound.val[1] << " ";
+            output << mask_bounds[i].lower_bound.val[2] << " ";
+            output << mask_bounds[i].upper_bound.val[0] << " ";
+            output << mask_bounds[i].upper_bound.val[1] << " ";
+            output << mask_bounds[i].upper_bound.val[2] << std::endl;
+        }
+        output.close();
+    } else {
+        std::ifstream input(robot_file_path);
+        while (!input.eof()) {
+            double h1, s1, v1, h2, s2, v2;
+            if (!(input >> h1 >> s1 >> v1 >> h2 >> s2 >> v2)) {
+                if (input.eof())
+                    break;
+                else {
+                    throw std::runtime_error("Malformed file: " + robot_file_path);
+                }
+            }
+            MaskBound tmp_mask_bound;
+            tmp_mask_bound.lower_bound = cv::Scalar(h1, s1, v1);
+            tmp_mask_bound.upper_bound = cv::Scalar(h2, s2, v2);
+            mask_bounds.push_back(tmp_mask_bound);
+        }
+    }
+}
+
+void Map::applyMasks(const cv::Mat &img_in, std::vector<MaskBound> &mask_bounds, cv::Mat &out_mask) {
     cv::Mat hsv_img;
     cv::cvtColor(img_in, hsv_img, cv::COLOR_BGR2HSV);
     cv::Mat masks[6];
@@ -73,4 +231,91 @@ void Map::applyMasks(const cv::Mat &img_in, std::vector<Map::MaskBound> &mask_bo
     }
 }
 
+void Map::findObstacles(const cv::Mat &mask_in, std::vector<Polygon> &obstacles, const double scale) {
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+
+    cv::findContours(mask_in, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    for (int i = 0; i < contours.size(); ++i) {
+        approxPolyDP(contours[i], approx_curve, 5, true);
+
+        Polygon scaled_contour;
+
+        for (const auto &pt : approx_curve) {
+            scaled_contour.emplace_back(pt.x / scale, pt.y / scale);
+        }
+
+        obstacles.push_back(scaled_contour);
+    }
+}
+
+void Map::findGate(const cv::Mat &mask_in, Polygon &gate, const double scale) {
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+
+    cv::findContours(mask_in, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    for (int i = 0; i < contours.size(); ++i) {
+        approxPolyDP(contours[i], approx_curve, 20, true);
+
+        if (approx_curve.size() != 4) {
+            continue;
+        }
+        Polygon scaled_contour;
+
+        for (const auto &pt : approx_curve) {
+            scaled_contour.emplace_back(pt.x / scale, pt.y / scale);
+        }
+        gate = scaled_contour;
+    }
+}
+
+void Map::findVictims(const cv::Mat &mask_in, std::vector<std::pair<int, Polygon>> &victims, const double scale) {
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+
+    cv::findContours(mask_in, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    for (int i = 0; i < contours.size(); ++i) {
+        approxPolyDP(contours[i], approx_curve, 3, true);
+        if (approx_curve.size() < 8) {
+            continue;
+        }
+        Polygon scaled_contour;
+
+        for (const auto &pt : approx_curve) {
+            scaled_contour.emplace_back(pt.x / scale, pt.y / scale);
+        }
+        // TODO: OCR
+
+        victims.push_back(std::make_pair(i, scaled_contour));
+    }
+}
+
+bool Map::findRobot(const cv::Mat &mask_in, Polygon &triangle, const double scale) {
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+
+    cv::findContours(mask_in, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    for (int i = 0; i < contours.size(); ++i) {
+        approxPolyDP(contours[i], approx_curve, 20, true);
+
+        if (approx_curve.size() != 3) {
+            continue;
+        }
+        Polygon scaled_contour;
+
+        for (const auto &pt : approx_curve) {
+            scaled_contour.emplace_back(pt.x / scale, pt.y / scale);
+        }
+        triangle = scaled_contour;
+    }
+    if (triangle.size() == 3) {
+        return true;
+    } else {
+        return false;
+    }
+}
 Map::~Map(){};
