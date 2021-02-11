0ef2f51d7dcde59a14132ac267602f6121e66542
arya <a.hasanli@yahoo.com>
2021-02-11 22:42:19 +0100

    Add config file

---
+++ include/config.hpp
@@ -0,0 +1,16 @@
+#define SHOW_OCR false
+#define SHOW_MASKS false
+#define SHOW_ROBOT false
+
+#define MISSION 1
+
+#define SHOW_SCALE 600.0
+
+#define OBST_MARGIN 0.10
+#define BORD_MARGIN 0.05
+
+#define NUM_HALTON_POINTS 499
+#define MAX_K_NEIGHBOURS 6
+#define MAP_SECTIONS 7
+
+#define K_P 40.0
